38210503fc4be768240c4f1b56df5f6bf8dce920
satya31vikas <103351337+satya31vikas@users.noreply.github.com>
2023-02-08 23:05:33 +0530

    Add files via upload

---
+++ STOP WATCH/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.SystemClock;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isRunning = false;
+    private long pauseOffset = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Chronometer chronometer = findViewById(R.id.chronometer);
+        Button btnStart = findViewById(R.id.button1);
+        Button btnPause = findViewById(R.id.button2);
+        Button btnReset = findViewById(R.id.button3);
+
+        chronometer.setFormat("Timer: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 60_000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "60 sec completed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRunning) {
+                    chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                    chronometer.start();
+                    isRunning = true;
+                }
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    chronometer.stop();
+                    pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    isRunning = false;
+                }
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.stop();
+                pauseOffset = 0;
+                isRunning = false;
+            }
+        });
+    }
+}
